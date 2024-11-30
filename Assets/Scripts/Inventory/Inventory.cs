e14dd0bac70b498d66e0b48df89bce10f62f5a57
Minneral <j3kawesome@yandex.ru>
2024-11-30 18:28:24 +0300

    Item and Inventory System
    Created basic item and inventory system: option of cteation and appending item to inventory

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int row = 3;
+    public int column = 9;
+    private int totalSlots;
+
+    public List<InventorySlot> slots;
+
+    // Метод для добавления предметов в инвентарь
+    public bool AddItem(Item newItem)
+    {
+        // Проверяем, можно ли застакать предмет с существующими
+        foreach (var slot in slots)
+        {
+            if (slot.item != null && slot.item.GetType() == newItem.GetType())
+            {
+                // Проверка, если предмет стакается
+                if (newItem.stackAmount > 1) // Предметы с возможностью стака добавляем
+                {
+                    int currentAmount = slot.amount;
+                    int stackSize = newItem.stackAmount;
+
+                    if (currentAmount < stackSize)
+                    {
+                        // Застакаем предметы, увеличив количество в текущем слоте
+                        slot.amount += 1;
+                        return true;
+                    }
+                    else
+                    {
+                        // Слот полностью заполнен, предмет не вмещается
+                        continue;
+                    }
+                }
+            }
+        }
+
+        // Если предмет не удалось застакать, находим пустой слот
+        foreach (var slot in slots)
+        {
+            if (slot.item == null)
+            {
+                slot.item = newItem;
+                slot.amount = 1; // Первоначально добавляем 1 предмет
+
+                return true;
+            }
+        }
+
+        // Если инвентарь полный
+        return false;
+    }
+
+    // Метод для вывода всех предметов в инвентаре
+    public void DisplayInventory()
+    {
+        foreach (var slot in slots)
+        {
+            if (slot.item != null)
+            {
+                if (slot.item.GetType() == typeof(FoodItem))
+                {
+                    FoodItem iii = slot.item as FoodItem;
+                    Debug.Log($"Item: {slot.item.itemName}, Amount: {slot.amount}, Type: {slot.item.GetType().Name}, Nutrition: {iii.nutritionValue}");
+                }
+
+                if (slot.item.GetType() == typeof(ResourceItem))
+                {
+                    ResourceItem iii = slot.item as ResourceItem;
+                    Debug.Log($"Item: {slot.item.itemName}, Amount: {slot.amount}, Type: {slot.item.GetType().Name}");
+                }
+            }
+        }
+    }
+
+    private void Start()
+    {
+        totalSlots = row * column;
+        slots = new List<InventorySlot>();
+
+        for (int i = 0; i < totalSlots; i++)
+        {
+            slots.Add(new InventorySlot());
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            DisplayInventory();
+        }
+    }
+}
+
+public class InventorySlot
+{
+    public Item item;
+    public int amount;
+}
